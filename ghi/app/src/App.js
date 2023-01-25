dcd6681f085932f01b70aba4ce7ef8e77ba5d1d2
Matthew <matthewlmai16@gmail.com>
2023-01-25 14:28:42 -0800

    merged main into mmai-branch

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -2,12 +2,133 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
 import { useEffect, useState } from 'react';
+import SalesPersonForm from './SalesPersonForm';
+import CustomerForm from './CustomerForm';
+import SalesRecordForm from './SalesRecordForm';
+
+import ManufacturersList from './ManufacturersList';
+import ManufacturerForm from './ManufacturersForm';
+import ModelsList from './ModelsList';
+import ModelsForm from './ModelsForm';
+
+import AppointmentsForm from './AppointmentsForm';
+import AppointmentsList from './AppointmentsList';
+import TechnicianForm from './TechnicianForm';
+import TechnicianList from './TechnicianList';
+import ServiceHistory from './ServiceHistory';
+
+
+function App() {
+  const [manufacturers, setManufacturers] = useState([]);
+  const [models, setModels] = useState([]);
+  const [appointments, setAppointments] = useState([]);
+  const [technicians, setTechnicians] = useState([]);
+
+  const getManufacturers = async () => {
+    const url = 'http://localhost:8100/api/manufacturers/';
+
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const data = await response.json();
+      const manufacturers = data.manufacturers;
+      setManufacturers(manufacturers);
+    }
+  }
+
+  const getModels = async () => {
+    const url = 'http://localhost:8100/api/models/';
+
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const data = await response.json();
+      const models = data.models;
+      setModels(models);
+    }
+  }
+
+  const getAppointments = async () => {
+    const url = 'http://localhost:8080/api/appointments/';
+
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const data = await response.json();
+      const appointments = data.appointments;
+      setAppointments(appointments);
+    }
+  }
+
+  const getTechnicians = async () => {
+    const url = 'http://localhost:8080/api/technicians/';
+
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const data = await response.json();
+      const technicians = data.technicians;
+      setTechnicians(technicians);
+    }
+  }
+
+  useEffect(() => {getManufacturers(); getModels(); getAppointments(); getTechnicians()}, [setManufacturers, setModels, setAppointments, setTechnicians]);
+
+  return (
+    <BrowserRouter>
+      <Nav />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/salesperson/new/" element={<SalesPersonForm />} />
+          <Route path="/customers/new/" element={<CustomerForm />} />
+          <Route path="/salesrecords/new/" element={<SalesRecordForm/>} />
+
+          {/* Routes for manufacturers */}
+          <Route path="manufacturers">
+            <Route path="list" element={<ManufacturersList  manufacturers={manufacturers} />} />
+            <Route path="new" element={<ManufacturerForm getManufacturers={getManufacturers} />} />
+          </Route>
+
+          {/* Routes for vehicle models */}
+          <Route path="models">
+            <Route path="list" element={<ModelsList  models={models} getModels={getModels} />} />
+            <Route path="new" element={<ModelsForm manufacturers={manufacturers} getModels={getModels} />} />
+          </Route>
+
+          {/* Routes for technicians */}
+          <Route path="technicians">
+            <Route path="list" element={<TechnicianList technicians={technicians} />} />
+            <Route path="new" element={<TechnicianForm getTechnicians={getTechnicians} />} />
+          </Route>
+
+          {/* Routes for appointments */}
+          <Route path="appointments">
+            <Route path="list" element={<AppointmentsList appointments={appointments} getAppointments={getAppointments} />} />
+            <Route path="new" element={<AppointmentsForm getAppointments={getAppointments} technicians={technicians} />} />
+          </Route>
+
+          {/* Route for service history */}
+          <Route path="services">
+            <Route path="history" element={<ServiceHistory appointments={appointments} getAppointments={getAppointments} setAppointments={setAppointments}/>} />
+          </Route>
+
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './MainPage';
+import Nav from './Nav';
+import { useEffect, useState } from 'react';
 import AppointmentsList from './AppointmentsList';
 import SalesPersonForm from './SalesPersonForm';
 import CustomerForm from './CustomerForm';
 import SalesRecordForm from './SalesRecordForm';
 import SalesPersonHistory from './SalesPersonHistory';
-import ModelsForm from './ModelsForm';
 
 import AppointmentsForm from './AppointmentsForm';
 import TechnicianForm from './TechnicianForm';
@@ -59,11 +180,9 @@ function App() {
           <Route path="customer" >
             <Route path="new" element={<CustomerForm />} />
           </Route>
-
-          {/* Routes for vehicle models */}
-          <Route path="models">
-            <Route path="list" element={<ModelsList  models={models} getModels={getModels} />} />
-            <Route path="new" element={<ManufacturerForm getManufacturers={getManufacturers} />} />
+          {/* Route for salesrecords */}
+          <Route path="salesrecords">
+            <Route path="new" element={<SalesRecordForm/>} />
           </Route>
 
           {/* Routes for technicians */}
@@ -75,7 +194,7 @@ function App() {
           {/* Routes for appointments */}
           <Route path="appointments">
             <Route path="list" element={<AppointmentsList appointments={appointments} getAppointments={getAppointments} />} />
-            <Route path="new" element={<AppointmentsForm getAppointments={getAppointments} technicians={technicians} />} />
+            <Route path="new" element={<AppointmentsForm getAppointments={getAppointments} getTechnicians={getTechnicians} />} />
           </Route>
 
           {/* Route for service history */}
