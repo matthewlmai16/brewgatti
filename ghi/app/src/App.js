0a88edcba0189c8a5bf22391e29b34ee4b9a656c
Matthew <matthewlmai16@gmail.com>
2023-01-25 14:53:30 -0800

    fixed merge conflicts

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -2,20 +2,52 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
 import { useEffect, useState } from 'react';
-import AppointmentsList from './AppointmentsList';
 import SalesPersonForm from './SalesPersonForm';
 import CustomerForm from './CustomerForm';
 import SalesRecordForm from './SalesRecordForm';
 import SalesPersonHistory from './SalesPersonHistory';
 
+import ManufacturersList from './ManufacturersList';
+import ManufacturerForm from './ManufacturersForm';
+import ModelsList from './ModelsList';
+import ModelsForm from './ModelsForm';
+
 import AppointmentsForm from './AppointmentsForm';
+import AppointmentsList from './AppointmentsList';
 import TechnicianForm from './TechnicianForm';
 import TechnicianList from './TechnicianList';
 import ServiceHistory from './ServiceHistory';
 
+
 function App() {
+  const [manufacturers, setManufacturers] = useState([]);
+  const [models, setModels] = useState([]);
   const [appointments, setAppointments] = useState([]);
   const [technicians, setTechnicians] = useState([]);
+
+  const getManufacturers = async () => {
+    const url = 'http://localhost:8100/api/manufacturers/';
+
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const data = await response.json();
+      const manufacturers = data.manufacturers;
+      setManufacturers(manufacturers);
+    }
+  }
+
+  const getModels = async () => {
+    const url = 'http://localhost:8100/api/models/';
+
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const data = await response.json();
+      const models = data.models;
+      setModels(models);
+    }
+  }
 
   const getAppointments = async () => {
     const url = 'http://localhost:8080/api/appointments/';
@@ -41,7 +73,7 @@ function App() {
     }
   }
 
-  useEffect(() => {getAppointments(); getTechnicians()}, [setAppointments, setTechnicians]);
+  useEffect(() => {getManufacturers(); getModels(); getAppointments(); getTechnicians()}, [setManufacturers, setModels, setAppointments, setTechnicians]);
 
   return (
     <BrowserRouter>
@@ -49,15 +81,33 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/salesperson/new/" element={<SalesPersonForm />} />
+          <Route path="/customers/new/" element={<CustomerForm />} />
+          <Route path="/salesrecords/new/" element={<SalesRecordForm/>} />
+
+          {/* Routes for manufacturers */}
+          <Route path="manufacturers">
+            <Route path="list" element={<ManufacturersList  manufacturers={manufacturers} />} />
+            <Route path="new" element={<ManufacturerForm getManufacturers={getManufacturers} />} />
+          </Route>
+
+          {/* Routes for vehicle models */}
+          <Route path="models">
+            <Route path="list" element={<ModelsList  models={models} getModels={getModels} />} />
+            <Route path="new" element={<ModelsForm manufacturers={manufacturers} getModels={getModels} />} />
+          </Route>
+
           {/* Route for salesperson */}
           <Route path="salesperson">
             <Route path="new" element={<SalesPersonForm />} />
             <Route path="history" element={<SalesPersonHistory />} />
           </Route>
+
           {/* Route for customer */}
           <Route path="customer" >
             <Route path="new" element={<CustomerForm />} />
           </Route>
+
           {/* Route for salesrecords */}
           <Route path="salesrecords">
             <Route path="new" element={<SalesRecordForm/>} />
@@ -72,7 +122,7 @@ function App() {
           {/* Routes for appointments */}
           <Route path="appointments">
             <Route path="list" element={<AppointmentsList appointments={appointments} getAppointments={getAppointments} />} />
-            <Route path="new" element={<AppointmentsForm getAppointments={getAppointments} getTechnicians={getTechnicians} />} />
+            <Route path="new" element={<AppointmentsForm getAppointments={getAppointments} technicians={technicians} />} />
           </Route>
 
           {/* Route for service history */}
@@ -85,5 +135,4 @@ function App() {
     </BrowserRouter>
   );
 }
-
 export default App;
--- ghi/app/src/AppointmentsForm.js
+++ ghi/app/src/AppointmentsForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 function AppointmentsForm({getAppointments, technicians}) {
 
