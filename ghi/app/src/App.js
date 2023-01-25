a69d5b48b8fb9a802c95a557696a16f529eec1c3
Matthew <matthewlmai16@gmail.com>
2023-01-25 11:35:44 -0800

    Completed ManufacturerList/Form components for Inventory MS

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -2,19 +2,35 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
 import { useEffect, useState } from 'react';
-import AppointmentsList from './AppointmentsList';
 import SalesPersonForm from './SalesPersonForm';
 import CustomerForm from './CustomerForm';
 import SalesRecordForm from './SalesRecordForm';
 
+import ManufacturersList from './ManufacturersList';
+import ManufacturerForm from './ManufacturersForm';
 import AppointmentsForm from './AppointmentsForm';
+import AppointmentsList from './AppointmentsList';
 import TechnicianForm from './TechnicianForm';
 import TechnicianList from './TechnicianList';
 import ServiceHistory from './ServiceHistory';
 
+
 function App() {
+  const [manufacturers, setManufacturers] = useState([]);
   const [appointments, setAppointments] = useState([]);
   const [technicians, setTechnicians] = useState([]);
+
+  const getManufacturers = async () => {
+    const url = 'http://localhost:8100/api/manufacturers/';
+
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const data = await response.json();
+      const manufacturers = data.manufacturers;
+      setManufacturers(manufacturers);
+    }
+  }
 
   const getAppointments = async () => {
     const url = 'http://localhost:8080/api/appointments/';
@@ -40,7 +56,7 @@ function App() {
     }
   }
 
-  useEffect(() => {getAppointments(); getTechnicians()}, [setAppointments, setTechnicians]);
+  useEffect(() => {getManufacturers(); getAppointments(); getTechnicians()}, [setManufacturers, setAppointments, setTechnicians]);
 
   return (
     <BrowserRouter>
@@ -51,6 +67,13 @@ function App() {
           <Route path="/salesperson/new/" element={<SalesPersonForm />} />
           <Route path="/customers/new/" element={<CustomerForm />} />
           <Route path="/salesrecords/new/" element={<SalesRecordForm/>} />
+
+          {/* Routes for manufacturers */}
+          <Route path="manufacturers">
+            <Route path="list" element={<ManufacturersList  manufacturers={manufacturers} />} />
+            <Route path="new" element={<ManufacturerForm getManufacturers={getManufacturers} />} />
+          </Route>
+
           {/* Routes for technicians */}
           <Route path="technicians">
             <Route path="list" element={<TechnicianList technicians={technicians} />} />
---
+++ ghi/app/src/ManufacturersForm.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+
+function ManufacturerForm({getManufacturers}) {
+
+    const [name, setManufacturerName] = useState('');
+
+
+
+    const handleNameChange = (event) => {
+        const value = event.target.value;
+        setManufacturerName(value);
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const data = {};
+
+        data.name = name;
+
+        const technicianUrl = 'http://localhost:8100/api/manufacturers/';
+        const fetchConfig = {
+            method: "post",
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
+
+        const response = await fetch(technicianUrl, fetchConfig);
+        if (response.ok) {
+            const newTechnician = await response.json();
+            console.log(newTechnician)
+
+            setManufacturerName('');
+            getManufacturers();
+        }
+    }
+
+
+
+    return (
+        <div className="row">
+            <div className="offset-3 col-6">
+                <div className="shadow p-4 mt-4">
+                <h1>Create a new manufacturer</h1>
+                <form onSubmit={handleSubmit} id="create-appointment-form">
+                    <div className="form-floating mb-3">
+                        <input onChange={handleNameChange} placeholder="Manufacturer name" required type="text" name = "name" id="name" className="form-control" value={name}/>
+                        <label htmlFor="name">Technician name</label>
+                    </div>
+                    <button className="btn btn-primary">Create</button>
+                </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default ManufacturerForm;
